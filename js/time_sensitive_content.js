9bd4e905292fee6f62ed12abbcb4b428316dfb22
chrisrz9 <chrisgrodriguez9@gmail.com>
2019-02-05 11:49:40 -0800

    commit index file update...

---
+++ js/time_sensitive_content.js
@@ -0,0 +1,8 @@
+/// JavaScript: time_sensitive_content
+
+var	date = new Date();
+
+var currentTime = new Date().getHours();
+if (7 <= currentTime && currentTime < 20) {
+	getElement.id("time_sensitive")
+}
