ff81c72c3053409d0e127fa9f74106f1f9e359ce
chrisrz9 <chrisgrodriguez9@gmail.com>
2019-05-21 10:40:25 -0700

    updating site, commit check.

--- js/time_sensitive_content.js
+++ js/time_sensitive_content.js
@@ -6,3 +6,31 @@ var currentTime = new Date().getHours();
 if (7 <= currentTime && currentTime < 20) {
 	getElement.id("time_sensitive")
 }
+
+
+
+
+if (hour >= 0)	{
+	document.getElementById('time_sensitive').innerHTML = "";
+}
+else if (hour >= 5)	{
+	document. getElenentById('time_sensitive').innerHTML = "Good Morning";
+}
+else if (hour >= 12)	{
+	document.getElementById('time_sensitive').innerHTML = "Good Afternoon";
+}
+else if (hour >=18)	{
+	document.getElementById('time_sensitive').innerHTML = "Good Evening";
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	(00 1 2 3 4 (5 6 7 8 9 10 11) (12 13 14 15 16 17) (18 19 20 21 22 23) 00
